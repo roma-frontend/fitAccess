5ffeeab21293675f78ccf7d85873289bd06b2052
roma-frontend <romangulanyan@gmail.com>
2025-06-05 22:38:42 +0400

    ok

--- components/about/timeline/TimelineConnector.tsx
+++ components/about/timeline/TimelineConnector.tsx
@@ -10,7 +10,7 @@ interface TimelineConnectorProps {
 
 export const TimelineConnector = memo<TimelineConnectorProps>(({ event }) => {
   return (
-    <div className="flex flex-col items-center lg:block hidden">
+    <div className="flex flex-col items-center lg:block">
       <div className={`w-6 h-6 bg-gradient-to-r ${event.yearGradient} rounded-full shadow-lg border-4 border-white relative z-10 timeline-dot`}>
         <div className="absolute inset-1 bg-white rounded-full opacity-30"></div>
       </div>
